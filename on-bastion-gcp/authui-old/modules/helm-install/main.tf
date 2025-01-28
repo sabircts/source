5b17f6f172dbbe754fd4da88c0140f600713a665
sabircts <sabir.husain@cognizant.com>
2025-01-28 14:43:13 +0530

    removed authui-old

--- on-bastion-gcp/authui-old/modules/helm-install/main.tf
+++
@@ -1,9 +0,0 @@
-resource "helm_release" "deployment" {
-  name             = var.deploymentname
-  chart            = var.chart
-  version          = var.chartversion != "" ? var.chartversion : null
-  repository       = var.chartrepository != "" ? var.chartrepository : null
-  namespace        = var.namespace
-  values           = [file("${path.module}/values.yaml")]
-  create_namespace = var.create_namespace
-}
--- on-bastion-gcp/authui-old/terraform.tfvars
+++
@@ -1,7 +0,0 @@
-project_id       = "cog01hy0xaqm3kfh4mr4t3em9ex2m"
-region           = "us-east1"
-gke_cluster_name = "fstest-gke-cluster"
-namespace        = "flowsource"
-create_namespace = false
-chart            = "authui-charts"
-deploymentname   = "authui-deployment"
